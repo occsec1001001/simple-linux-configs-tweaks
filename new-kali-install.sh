2a7a1b2c141b8852c60b33460764b217f5999bbe
occsec1001001 <74115178+occsec1001001@users.noreply.github.com>
2020-11-08 22:26:44 +0000

    Create new-kali-install.sh

---
+++ new-kali-install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -x
+
+rm -r Music/
+rm -r Templates/
+rm -r Videos/
+rm -r Pictures/
+rm -r Public/
+mkdir git
+mkdir general
+mkdir webserv
+mkdir ftpserv
+sudo apt-get update -y
+sudo apt install -y openvpn dialog python3-pip python3-setuptools
+sudo pip3 install protonvpn-cli
+sudo pip3 install protonvpn-cli --upgrade
+sudo apt-get install gdebi-core
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo mv google-chrome-stable_current_amd64.deb /opt/google-chrome.deb
+sudo gdebi /opt/google-chrome.deb 
+sudo apt-get install -y joplin
+sudo systemctl enable postgresql
+sudo systemctl enable ssh
+sudo apt-get install -y python3
+sudo apt install software-properties-common apt-transport-https curl
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt-get update
+sudo apt install code
+curl https://sdk.cloud.google.com | bash
+sleep 10
+exec -l $SHELL
+sudo apt install -y sublist3r
+sudo apt-get install -y timeshift
+sudo apt install -y software-center
+sudo apt=get update
+sudo apt-get autoclean
+sudo apt-get autoremove
