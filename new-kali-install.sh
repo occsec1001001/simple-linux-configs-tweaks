2fe374ec033f54dabbb73aa054d261081cc2f782
occsec1001001 <74115178+occsec1001001@users.noreply.github.com>
2020-11-15 15:30:41 +0000

    Update new-kali-install.sh

--- new-kali-install.sh
+++ new-kali-install.sh
@@ -11,29 +11,34 @@ mkdir git
 mkdir general
 mkdir webserv
 mkdir ftpserv
+mkdir current
+mkdir current/wip
+mkdir current/archive
 sudo apt-get update -y
 sudo apt install -y openvpn dialog python3-pip python3-setuptools
-sudo pip3 install protonvpn-cli
-sudo pip3 install protonvpn-cli --upgrade
-sudo apt-get install gdebi-core
+sudo pip3 install protonvpn-cli -y
+sudo pip3 install protonvpn-cli --upgrade -y
+sudo apt-get install gdebi-core -y
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo mv google-chrome-stable_current_amd64.deb /opt/google-chrome.deb
 sudo gdebi /opt/google-chrome.deb 
 sudo apt-get install -y joplin
-sudo systemctl enable postgresql
-sudo systemctl enable ssh
+#sudo systemctl enable postgresql
+#sudo systemctl enable ssh
 sudo apt-get install -y python3
-sudo apt install software-properties-common apt-transport-https curl
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt-get update
-sudo apt install code
-curl https://sdk.cloud.google.com | bash
-sleep 10
-exec -l $SHELL
+sudo apt-get install python3-pip -y
+#sudo apt install software-properties-common apt-transport-https curl -y
+#curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+#sudo apt-get update
+#sudo apt install code
+#curl https://sdk.cloud.google.com | bash
+#sleep 10
+#exec -l $SHELL
 sudo apt install -y sublist3r
 sudo apt-get install -y timeshift
-sudo apt install -y software-center
+#sudo apt install -y software-center
+sudo apt install -y zsh
 sudo apt=get update
 sudo apt-get autoclean
 sudo apt-get autoremove
