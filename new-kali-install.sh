a790398c995f5c6367cfe240d3ad7a0a29ad1c85
occsec1001001 <74115178+occsec1001001@users.noreply.github.com>
2020-12-17 14:10:47 -0500

    Update new-kali-install.sh

--- new-kali-install.sh
+++ new-kali-install.sh
@@ -41,10 +41,10 @@ sudo dpkg -i Downloads/Nessus-8.13.0-debian6_amd64.deb
 sudo rm Downloads/Nessus-8.13.0-debian6_amd64.deb
 sudo apt-get install linux headers-$(uname -r)
 sudo usermod -a -G sudo occami
-sudo apt-get install Terminator -y
-sudo mkdir -p ~/.config/terminator/plugins
-sudo wget https://raw.githubusercontent.com/msudgh/terminator-search/master/terminator_search.py
-sudo mv terminator_search.py ~/.config/terminator/plugins/terminator_search.py
+#sudo apt-get install Terminator -y
+#sudo mkdir -p ~/.config/terminator/plugins
+#sudo wget https://raw.githubusercontent.com/msudgh/terminator-search/master/terminator_search.py
+#sudo mv terminator_search.py ~/.config/terminator/plugins/terminator_search.py
 #git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 #sh ~/.vim_runtime/install_awesome_vimrc.sh
 sudo git clone https://github.com/vulnersCom/nmap-vulners.git /usr/share/nmap/scripts/
@@ -54,7 +54,8 @@ sudo apt-get install npm -y
 sudo apt-get install -y python3
 sudo apt-get install python3-pip -y
 sudo apt install software-properties-common apt-transport-https curl -y
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+#curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt-get update
 sudo apt install code
